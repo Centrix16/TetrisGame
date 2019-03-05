dce056daab549a7ef825a2ceb5a5924e758eb5a7
Centrix16 <48253248+Centrix16@users.noreply.github.com>
2019-03-05 19:04:18 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "tg.h"
+#include <cstdlib>
+using namespace std;
+
+typedef TetrisGame Tg;
+
+int main()
+{
+	Tg tg(10, 10);
+	tg.SetDet(0);
+	tg.show();
+	char choice = 'n';
+	while (choice != 'e')
+	{
+		cin >> choice;
+		tg.Move(choice);
+		//system("clear");
+		tg.show();
+	}
+	return 0;
+}
